a1bb09202a6b278cede05701c29098122b0f20a7
TheSky04 <furkann.bas34@gmail.com>
2024-01-03 22:19:59 +0300

    menu icon is added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageButtons from "./LanguageButtons";
-import styles from "../styles/Navbar.module.css";
 import { configs } from "../config";
 import { FiMail } from "react-icons/fi";
 import {
@@ -13,122 +12,95 @@ import {
   FaTimes,
 } from "react-icons/fa";
 import BerkoLogo from "../assets/berkologo.png";
-import { useState, useEffect } from "react";
 
 function Navbar() {
   const { t } = useTranslation();
-  const [isMobile, setIsMobile] = useState(false);
 
-  useEffect(() => {
-    const handleResize = () =>
-      window.innerWidth < 1000 ? setIsMobile(true) : setIsMobile(false);
+  const headerEl = document.querySelector(".header");
 
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const handleMenuOpen = () => {
-    console.log("okey");
+  const handleMenuOpen = function () {
+    headerEl.classList.toggle("nav-open");
+    console.log(headerEl);
   };
 
   return (
-    <>
-      <div className={styles.navTop}>
-        <div className={styles.navTopLeft}>
-          <div className={styles.navTopLeftEmail}>
+    <header className="header">
+      <div className="navTop">
+        <div className="navTopLeft">
+          <div className="navTopLeftEmail">
             <FiMail color="#fff" />
             <p>{configs.email}</p>
           </div>
-          <div className={styles.navTopLeftAddress}>
+          <div className="navTopLeftAddress">
             <FaMapMarkerAlt color="#fff" />
             <p>{configs.address} </p>
           </div>
         </div>
-        <div className={styles.navTopCenter}>
-          <div className={styles.navTopCenterSocial}>
+        <div className="navTopCenter">
+          <div className="navTopCenterSocial">
             <Link to={configs.instagramAdress}>
               <FaInstagram
                 size={40}
                 color="#fff"
-                className={styles.navTopCenterSocialInstagram}
+                className="navTopCenterSocialInstagram"
               />
             </Link>
             <Link to={configs.facebookAdress}>
               <FaFacebook
                 size={40}
                 color="#fff"
-                className={styles.navTopCenterSocialFacebook}
+                className="navTopCenterSocialFacebook"
               />
             </Link>
           </div>
         </div>
       </div>
-      <nav className={styles.nav}>
-        <div className={styles.navLogo}>
+      <nav className="nav">
+        <div className="navLogo">
           <img src={BerkoLogo} alt="berko logo" />
         </div>
-        {!isMobile && (
-          <div className={styles.navLeft}>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-            >
-              {t("Home")}
-            </NavLink>
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-            >
-              {t("About")}
-            </NavLink>
-            <NavLink
-              to="/contact"
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-            >
-              {t("Contact")}
-            </NavLink>
-            <NavLink
-              to="/products"
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-            >
-              {t("Products")}
-            </NavLink>
-            <NavLink
-              to="/production"
-              className={({ isActive }) =>
-                isActive ? styles.active : styles.navLink
-              }
-            >
-              {t("Production")}
-            </NavLink>
-          </div>
-        )}
+        <div className="navLeft">
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "active" : "navLink")}
+          >
+            {t("Home")}
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={({ isActive }) => (isActive ? "active" : "navLink")}
+          >
+            {t("About")}
+          </NavLink>
+          <NavLink
+            to="/contact"
+            className={({ isActive }) => (isActive ? "active" : "navLink")}
+          >
+            {t("Contact")}
+          </NavLink>
+          <NavLink
+            to="/products"
+            className={({ isActive }) => (isActive ? "active" : "navLink")}
+          >
+            {t("Products")}
+          </NavLink>
+          <NavLink
+            to="/production"
+            className={({ isActive }) => (isActive ? "active" : "navLink")}
+          >
+            {t("Production")}
+          </NavLink>
+        </div>
 
-        {!isMobile && (
-          <div className={styles.navRight}>
-            <LanguageButtons />
-          </div>
-        )}
-
-        {isMobile && (
-          <div className={styles.iconContainer}>
-            <FaBars className={styles.menuIcon} onClick={handleMenuOpen} />
-            <FaTimes className={styles.closeIcon} />
-          </div>
-        )}
+        <div className="navRight">
+          <LanguageButtons />
+        </div>
+        <div className="iconContainer" onClick={handleMenuOpen}>
+          <FaBars className="menuIcon" />
+          <FaTimes className="closeIcon" />
+        </div>
       </nav>
-    </>
+    </header>
   );
 }
 
