8407b4f109b7836ca57083661956913243aa8588
TheSky04 <furkann.bas34@gmail.com>
2024-01-16 20:28:21 +0300

    navbar html is added.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,36 @@ function Navbar({ isMobile }) {
 
   return (
     <header className={`${navOpen} header`}>
+      <div className="navTop">
+        <div className="navTopLeft">
+          <div className="navTopLeftEmail">
+            <FiMail color="#fff" />
+            <p>{configs.email}</p>
+          </div>
+          <div className="navTopLeftAddress">
+            <FaMapMarkerAlt color="#fff" />
+            <p>{configs.address} </p>
+          </div>
+        </div>
+        <div className="navTopCenter">
+          <div className="navTopCenterSocial">
+            <Link to={configs.instagramAdress}>
+              <FaInstagram
+                size={40}
+                color="#fff"
+                className="navTopCenterSocialInstagram"
+              />
+            </Link>
+            <Link to={configs.facebookAdress}>
+              <FaFacebook
+                size={40}
+                color="#fff"
+                className="navTopCenterSocialFacebook"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
       <nav className="nav">
         <div className="navLogo">
           <img src={BerkoLogo} alt="berko logo" />
